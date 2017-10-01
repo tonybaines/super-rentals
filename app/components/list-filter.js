c2b19ec1378861938a12e4ab88da3229478b04dd
Tony Baines <tony.baines.1@gmail.com>
2017-10-01 17:36:23 +0100

    Add new files

---
+++ app/components/list-filter.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['list-filter'],
+  value: '',
+
+  init() {
+    this._super(...arguments);
+
+    this.get('filter')('').then((allResults) => {
+      this.set('results', allResults.results);
+    });
+  },
+
+  actions: {
+    handleFilterEntry() {
+      let filterInputValue = this.get('value');
+      let filterAction = this.get('filter');
+      filterAction(filterInputValue).then((filterResults) => {
+        if (filterResults.query === this.get('value')) {
+          this.set('results', filterResults.results);
+        }
+      });
+    }
+  }
+
+});
---
+++ app/controllers/rentals.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    filterByCity(param) {
+      if (param !== '') {
+        return this.get('store').query('rental', { city: param }).then((results) => {
+          return { query: param, results: results };
+        });
+      } else {
+        return this.get('store').findAll('rental').then((results) => {
+          return { query: param, results: results };
+        });
+      }
+    }
+  }
+});
